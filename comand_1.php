7d4b7254ee3658f4d04be8ef4e1cbad879bdca63
yudsss <nziidhaa090920@gmail.com>
2022-09-28 15:38:51 +0700

    280922

---
+++ comand_1.php
@@ -0,0 +1,4 @@
+<?php
+"hallo dunia ini saya belajar php
+ini program php yang pertama"
+?>
---
+++ command_2.php
@@ -0,0 +1,5 @@
+<?php
+echo "hallo dunia ini saya belajar php\n";
+echo"ini baris kedua\n" ;
+echo"ini baris ketiga\n";
+?>
