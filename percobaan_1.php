a722e52789970ee9a0cb08a33d656f5e420dfcf5
yudsss <nziidhaa090920@gmail.com>
2022-09-28 19:43:11 +0700

    malem

---
+++ percobaan_1.php
@@ -0,0 +1,10 @@
+<?php
+echo "<p> hello world</p>";
+/*
+this is a multi-line comment block 
+that spans over 
+multiple lines 
+*/
+echo "<p>saya belajar php</p>";
+echo "<p>ini adalah program pertama saya";
+?>
