df7bac270d2fd0737ca4e4e53312022f206777ea
yudsss <nziidhaa090920@gmail.com>
2022-10-05 20:20:26 +0700

    rabu

--- variable_2.php
+++ variable_2.php
@@ -1,16 +1,33 @@
 <?php
+
+// const
+define("phi",3.14);
+
+// sisi miring kerucut
+$a = pow(5, 2);
+$b = pow(7.2,2);
+$c = sqrt($a=$b);
+echo 'sisi miring kerucut ='.round($c,1)."m \n";
+
 // volume tabung
-$phi = 3.14;
 $r = 7.2;
-$tinggi = 20;
-$volume_tabung = $phi * $r * $r * $tinggi;
-echo $volume_tabung;
-echo "\n";
+$tTabung = 20;
+$volTabung = phi*pow($r, 2)*$tTabung;
+echo 'volume tabung ='.round($volTabung,1)."m3\n";
+
 // volume kerucut
-$pi = 1/3;
-$r = 7.2;
-$t = 5;
-$volume_krucut = $pi / $r * $r * $t;
-echo $volume_krucut;
-echo "\n";
+$tKerucut = 5;
+$volKerucut = (phi*pow($r,2)*$tKerucut);
+echo 'volume kerucut ='.round($volKerucut,1)."m3\n";
+
+// luas alas yang hilang
+$lAlas = phi*pow($r,2) / 100;
+echo 'luas alas hilang'.round($lAlas,1)."m3\n";
+
+// total
+$totalVolume = $volTabung + $volKerucut - $lAlas;
+echo 'total volume ='.round($totalVolume,1)."m3\n";
+
+
+
 ?>
