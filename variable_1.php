96c4cb72e4561194ae27e6d1ec6acf73d4ca36d7
yudsss <nziidhaa090920@gmail.com>
2022-10-05 15:24:54 +0700

    rabu

---
+++ variable_1.php
@@ -0,0 +1,8 @@
+<?php
+// luas persegi panjang
+$panjang = 10;
+$lebar = 10;
+$luas_persegi_panjang = $panjang * $lebar;
+echo $luas_persegi_panjang;
+echo "\n";
+?>
---
+++ variable_2.php
@@ -0,0 +1,16 @@
+<?php
+// volume tabung
+$phi = 3.14;
+$r = 7.2;
+$tinggi = 20;
+$volume_tabung = $phi * $r * $r * $tinggi;
+echo $volume_tabung;
+echo "\n";
+// volume kerucut
+$pi = 1/3;
+$r = 7.2;
+$t = 5;
+$volume_krucut = $pi / $r * $r * $t;
+echo $volume_krucut;
+echo "\n";
+?>
